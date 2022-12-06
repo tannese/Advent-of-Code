3c0e9ad02deb0869d19d0b5a2589650377a17f4c
Tony Annese <tannese@gmail.com>
2022-12-06 00:44:03 -0800

    Updated makefolders utility to create templates

--- 2022/utilities/makefolders.py
+++ 2022/utilities/makefolders.py
@@ -6,16 +6,31 @@
 
 import os
 import datetime
+import subprocess
 
+path = '/Users/tony.annese/Documents/VS Code/Advent of Code'
 today = datetime.date.today()
 year = str(today.year)
-os.mkdir(year)
-
+files = ['1.py', 'input.test.txt', 'input.txt', 'README.md']
+python = subprocess.run(["which", "python3"], capture_output=True, text=True).stdout
 
 i = 1
 while (i < 26):
-    day = f"{year}/Day{i}"
-    os.mkdir(day)
+    day = f"{path}/{year}/Day{i}"
+    os.makedirs(day)
+    for file in files:
+        fp = open(os.path.join(path, day, file), 'x')
+        fp.close()
+        if file == '1.py':
+            body = f"#!{python}\n#file = 'input.txt'\nfile = 'input.test.txt'\n\nif __name__ == '__main__':"
+            fp = open(os.path.join(path, day, file), 'w')
+            fp.write(body)
+            fp.close()
+        elif file == 'README.md':
+            body = f"## Day {i}\n"
+            fp = open(os.path.join(path, day, file), 'w')
+            fp.write(body)
+            fp.close()
     i += 1
 
  
