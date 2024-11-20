3aba17ca47fc062884a4adae2854245c5d54e7fa
Tony Annese <tannese@gmail.com>
2024-11-20 02:18:11 -0800

    Completed Day 2 Part 2

--- 2023/Day2/1.py
+++ 2023/Day2/1.py
@@ -6,7 +6,6 @@ file = 'input.txt'
 def part1(inFile):
     validGames = []
     maxCubes = {'red':12, 'green':13, 'blue':14}
-    # print(inFile)
     for line in inFile:
         blocks = {'red':0, 'blue':0, 'green':0}
         gameset = line.split(':')
@@ -27,9 +26,28 @@ def part1(inFile):
             validGames.append(game)
     return sum(validGames)
 
+def part2(inFile):
+    totalPower = 0
+    for line in inFile:
+        blocks = {'red':0, 'blue':0, 'green':0}
+        gameset = line.split(':')
+        sets = gameset[1].split(';')
+        for set in sets:
+            elements = set.split(',')
+            for element in elements:
+                color = element.split()[1]
+                quantity = int(element.split()[0])
+                if blocks[color] < quantity:
+                    blocks[color] = quantity
+        gamePower = blocks['red'] * blocks['green'] * blocks['blue']
+        totalPower += gamePower
+    return totalPower
+
 if __name__ == '__main__':
     with open(file, 'r') as f:
         output = f.read().strip().split("\n")
 
 p1 = part1(output)
+p2 = part2(output)
 print(f'Part 1: {p1}')
+print(f'Part 2: {p2}')
