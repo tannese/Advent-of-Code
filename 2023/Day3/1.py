07fc5c267e5f008defd8bfc989c143495baed60c
Tony Annese <tannese@gmail.com>
2024-12-01 18:22:45 -0800

    Intitial 2024 Setup and Day 1 Setup

--- 2023/Day3/1.py
+++ 2023/Day3/1.py
@@ -3,4 +3,27 @@
 #file = 'input.txt'
 file = 'input.test.txt'
 
+def findSymbols(inFile):
+    symbols = []
+    for line in output:
+        for char in line:
+            if not char.isdigit() and char != '.' and char not in symbols:
+                symbols.append(char)
+    return symbols  
+
+def part1(symbols, inFile):
+    for line in inFile:
+        for char in line:
+            if char.isdigit():
+                print(char.index())
+    return
+
 if __name__ == '__main__':
+    with open(file, 'r') as f:
+        output = f.read().strip().split("\n")
+
+
+    symbols = findSymbols(output)
+    p1 = part1(symbols, output)
+
+
